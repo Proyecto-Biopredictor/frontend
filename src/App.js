f6562890f4dd3de0ac5af0da5498a14c6dbd5d35
ErickElizondo <erickelizondoalt20@gmail.com>
2021-09-05 00:03:58 -0600

    Can upload file and read its content as object

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import FileManager from './components/FileManager';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a> */}
         <h1>Hello World!</h1>
+        <FileManager/>
       </header>
     </div>
   );
