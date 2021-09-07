c46d0a93c859dadaec618bc33906f7a3d8a1a69e
ErickElizondo <erickelizondoalt20@gmail.com>
2021-09-06 19:27:21 -0600

    BIOP-26 setup page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,55 @@ import logo from './logo.svg';
 import './App.css';
 import FileManager from './components/FileManager';
 import Register from './components/Register';
+import Header from "./components/Header";
+import SideMenu from "./components/SideMenu";
+import { makeStyles, CssBaseline, createMuiTheme, ThemeProvider } from '@material-ui/core';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: '#3c44b126'
+    },
+    secondary: {
+      main: "#f83245",
+      light: '#f8324526'
+    },
+    background: {
+      default: "#f4f5fd"
+    },
+  },
+  overrides: {
+    MuiAppBar: {
+      root: {
+        transform: 'translateZ(0)'
+      }
+    }
+  },
+  props: {
+    MuiIconButton: {
+      disableRipple: true
+    }
+  }
+})
+
+
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: '320px',
+    width: '100%'
+  }
+})
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
+    <ThemeProvider theme={theme}>
+      <Header />
+
+      <div className={classes.appMain}>
+        <SideMenu />
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,10 +64,11 @@ function App() {
           Learn React
         </a> */}
         <h1>Hello World!</h1>
-        <FileManager/>
+        <FileManager />
         <Register />
-      </header>
-    </div>
+      </div>
+      <CssBaseline />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { AppBar, Toolbar, Grid, InputBase, IconButton, Badge, makeStyles, CssBaseline } from '@material-ui/core'
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import SearchIcon from '@material-ui/icons/Search';
+import Typography from '@material-ui/core/Typography';
+
+const drawerWidth = 240;
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerContainer: {
+        overflow: 'auto',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+}));
+
+export default function Header() {
+
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                    <Typography variant="h6" noWrap>
+                        Clipped drawer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
