69c4776de228c782abc78aa5debad508e35e5715
maurigamg <44620109+maurigamg@users.noreply.github.com>
2021-11-16 10:46:50 -0600

    histogram new design

--- src/pages/Graphics/ShowHistogram.js
+++ src/pages/Graphics/ShowHistogram.js
@@ -7,7 +7,7 @@ export default function ShowHistogram(props) {
     const options = {
         scales: {
             x: {
-                type: 'linear'
+                type: 'linear',
             },
             y: {
                 beginAtZero: true
@@ -21,7 +21,7 @@ export default function ShowHistogram(props) {
                         let index = context.dataIndex;
                         let dataset = context.dataset.data;
                         let interval = "Intervalo: [";
-                        interval += dataset[index - 1].x;
+                        interval +=  index > 0 ? dataset[index - 1].x : " ";
                         interval += ", " + context.parsed.x + "[";
                         let frequence = "Frecuencia: " + context.parsed.y;
                         return [interval, frequence];
@@ -46,14 +46,14 @@ export default function ShowHistogram(props) {
 
         let parseData = [];
         if (classes > 0) {
-            range = dataVariable.at(-1) - dataVariable.at(0);
+            range = parseFloat(dataVariable.at(-1)) - parseFloat(dataVariable.at(0));
             if (classes !== 0) {
                 amplitude = Math.round(range / classes);
             }
-            let interval = Math.round(dataVariable[0] + amplitude);
+            let interval = Math.round(parseFloat(dataVariable.at(0)) + amplitude);
             for (let index = 0; index < dataVariable.length; index++) {
                 let frequence = 0;
-                while (dataVariable[index] < interval) {
+                while (parseFloat(dataVariable[index]) < interval) {
                     frequence++;
                     index++;
                 }
@@ -66,25 +66,11 @@ export default function ShowHistogram(props) {
             datasets: [{
                 label: key,
                 data: parseData,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(255, 205, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(201, 203, 207, 0.2)'
-                ],
-                borderColor: [
-                    'rgb(255, 99, 132)',
-                    'rgb(255, 159, 64)',
-                    'rgb(255, 205, 86)',
-                    'rgb(75, 192, 192)',
-                    'rgb(54, 162, 235)',
-                    'rgb(153, 102, 255)',
-                    'rgb(201, 203, 207)'
-                ],
-                borderWidth: 1
+                backgroundColor: '#8ecae6',
+                borderColor: '#219ebc',
+                borderWidth: 1,
+                categoryPercentage: 1,
+                barPercentage: 1
             }]
         };
         configs.push(config);
